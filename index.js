4382ec4cefd313fd346b6f9c0e304675f621406b
c00kamunga <tsothalanti2@yahoo.com>
2020-08-05 17:45:57 -0700

    added helper functions to project

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const server = express()
 const port = 2319
+
+server.use(express.json())
+
+server.listen(port, () => {
+    console.log(`\n *** server is listening on port ${port} *** \n`)
+})
