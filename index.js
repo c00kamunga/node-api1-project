a152383105be5858713134d32c2d8e6170c2b49c
c00kamunga <tsothalanti2@yahoo.com>
2020-08-05 19:03:13 -0700

    fixed both getUserbyId and post user endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const server = express()
 const db = require('./db')
+const { json } = require('express')
 const port = 7654
 
 server.use(express.json())
@@ -9,11 +10,13 @@ server.listen(port, () => {
     console.log(`\n *** server is listening on port ${port} *** \n`)
 })
 
-
+//endpoint for testing API
 server.get('/', (req, res) => {
     res.send("hello world")
 })
 
+
+//end point for getting a list of all the users
 server.get('/api/users', async(req, res) => {
     try{
         const users = await db.getUsers()
@@ -24,3 +27,49 @@ server.get('/api/users', async(req, res) => {
         .json({ error: 'the user information could not be retrieved'})
     }
 })
+
+
+//endpoint for posting a single user
+server.post('/api/users', (req, res) => {
+    if(req.body.name && req.body.bio) {
+        const newUser = db.createUser({name: req.body.name, bio: req.body.bio})
+        const users = db.getUsers()
+        if(users){
+            res.json(users)
+            res.status(201)
+        }
+        else {
+            res
+            .status(500)
+            .json({ errorMessage: "error saving user to database"})
+        }
+    } else {
+        res
+        .status(400)
+        .json({ errorMessage: "no name or bio for user" })
+    }
+})
+
+//endpoint for getting single user with specified ID
+server.get('/api/users/:id', (req, res) => {
+   const id = req.params.id
+   const user = db.getUserById(id)
+
+   if (user){
+       try{
+           res
+           .json(user)
+       }
+       catch (error){
+           res
+           .status(500)
+           .json ({ errorMessage: "user information not found"})
+       }
+   } else {
+       res
+       .status(404)
+       .json({ message: "user with specified ID does not exist"})
+   }
+})
+
+
