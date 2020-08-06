0dac109c069bb1c740638278a895f361f2fb5f33
c00kamunga <tsothalanti2@yahoo.com>
2020-08-05 17:50:28 -0700

    added get end point for testing api

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const server = express()
-const port = 2319
+const port = 7654
 
 server.use(express.json())
 
 server.listen(port, () => {
     console.log(`\n *** server is listening on port ${port} *** \n`)
+})
+
+
+server.get('/', (req, res) => {
+    res.send("hello world")
 })
